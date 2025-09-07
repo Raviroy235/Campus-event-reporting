b1a6998845058cdc3a49c54c10c99d41ce1707d2
Raviroy235 <naradasuraviteja235@gmail.com>
2025-09-07 12:09:02 +0530

    Delete create_db.py

--- create_db.py
+++
@@ -1,9 +0,0 @@
-from app.database import Base, engine
-import app.models as models
-
-def create():
-    Base.metadata.create_all(bind=engine)
-    print("Database & tables created (events.db)")
-
-if __name__ == "__main__":
-    create()
